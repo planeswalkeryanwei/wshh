e6aa81d5aae3be217a5f4c22682e65c37be9d60c
Your Name <you@example.com>
2022-03-01 17:39:23 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { Input, Table, Space, Popconfirm } from 'antd'
+import axios from 'axios'
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Search } = Input
+
+class App extends React.Component {
+  state = {
+    list: [],
+    columns: [
+      {
+        title: '任务编号',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '任务名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '任务描述',
+        dataIndex: 'des',
+        key: 'des',
+      },
+      {
+        title: '操作',
+        dataIndex: 'do',
+        key: 'do',
+        render: (text, record) => (
+          <Space size="middle">
+            <Popconfirm title="确定要删除吗?"
+              onConfirm={() => this.handleDelete(record.id)}>
+              <a href="#">删除</a>
+            </Popconfirm>
+          </Space>
+        ),
+      },
+    ]
+  }
+
+  // 搜索
+  onSearch = async (value) => {
+    const res = await axios.get(`http://localhost:3001/data/?q=${value}`)
+    this.setState({
+      list: res.data
+    })
+  }
+  // 删除
+  handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/data/${id}`)
+    this.loadList()
+  }
+  // 加载列表
+  loadList = async () => {
+    const res = await axios.get('http://localhost:3001/data')
+    console.log(res)
+    this.setState({
+      list: res.data
+    })
+  }
+  componentDidMount () {
+    // 发送接口请求
+    this.loadList()
+  }
+  render () {
+    return (
+      <div className="container">
+        <div className="search-box">
+          <Search
+            placeholder="请输入关键词"
+            allowClear
+            enterButton="搜索"
+            size="large"
+            onChange={this.inputChange}
+            value={this.state.keyword}
+            onSearch={this.onSearch}
+          />
+        </div>
+        <Table bordered dataSource={this.state.list} columns={this.state.columns} pagination={false} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
