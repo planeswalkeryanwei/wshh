2d4e672a940378fca577e6f696868eff5e843c86
planeswalkeryanwei <Yanwei22750>
2022-05-11 17:11:23 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,69 +1,35 @@
-import { Input, Table, Space, Popconfirm } from 'antd'
-import React from 'react'
-import './App.css'
-
-const { Search } = Input
-
-class App extends React.Component {
-  state = {
-    list: [],
-    columns: [
-      {
-        title: '任务编号',
-        dataIndex: 'id',
-        key: 'id',
-      },
-      {
-        title: '任务名称',
-        dataIndex: 'name',
-        key: 'name',
-      },
-      {
-        title: '任务描述',
-        dataIndex: 'des',
-        key: 'des',
-      },
-      {
-        title: '操作',
-        dataIndex: 'do',
-        key: 'do',
-        render: (text, record) => (
-          <Space size="middle">
-            <Popconfirm title="确定要删除吗?"
-              onConfirm={() => this.handleDelete(record.id)}>
-              <a href="#">删除</a>
-            </Popconfirm>
-          </Space>
-        ),
-      },
-    ]
-  }
-
-  // 搜索
-
-  // 删除
-
-  // 加载列表
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
-  render () {
+import Head from './componentEveryOne/Head';
+import Left from './componentEveryOne/Left';
+
+import Login from './component/Login';
+import Main from './component/Main';
+import New from './component/New';
+import Demo1 from './component/Demo1';
+import Jieshou from './component/Jieshou';
+import Modal from './component/Modal';
+import Check from './component/Check';
+function App(){
     return (
-      <div className="container">
-        <div className="search-box">
-          <Search
-            placeholder="请输入关键词"
-            allowClear
-            enterButton="搜索"
-            size="large"
-            onChange={this.inputChange}
-            value={this.state.keyword}
-            onSearch={this.onSearch}
-          />
-        </div>
-        <Table bordered dataSource={this.state.list} columns={this.state.columns} pagination={false} />
-      </div>
+
+        <>
+        <BrowserRouter><Head/><Left/>
+            <Routes>
+            {/* <Route path='/jieshou' component={<Jieshou />} /> */}
+                <Route path='/' element={<Login />} />
+                <Route path='/demo1' element={<Demo1 />} />
+                <Route path='/jieshou' element={<Jieshou />} />
+                <Route path='/modal' element={<Modal />} />
+                <Route path='/main' element={<Main />} />
+                <Route path='/head' element={<Head />} />
+                <Route path='/new' element={<New />} />
+                <Route path='/left' element={<Left />} />
+                <Route path='/Check' element={<Check />} />
+            </Routes>
+        </BrowserRouter></>
     )
-  }
 }
 
 export default App
